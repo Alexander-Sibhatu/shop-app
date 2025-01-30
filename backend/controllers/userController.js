b75fb938691c8e62f0200f04a986a4849e253427
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-01-30 17:14:15 +0000

    admin login controller implemented

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -91,8 +91,25 @@ const registerUser = async (req, res) => {
 }
 
 // Route for admin login
-const adminLogin = async (res, req) => {
+const adminLogin = async (req, res) => {
 
+    try {
+        
+        const {email, password} = req.body
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD ) {
+            const token = jwt.sign(email+password, process.env.JWT_SECRET_KEY)
+            res.json({success: true, token})
+        } else {
+            res.json({success: false, message: "Invalid email/password"})
+        }
+    } catch (error) {
+        console.log(error)
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
 }
 
 
