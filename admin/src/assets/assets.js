fead3afbf4b01be724a5d6b6c5da19e208db0112
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-01-30 17:16:04 +0000

    admin panel

---
+++ admin/src/assets/assets.js
@@ -0,0 +1,10 @@
+// import logo from './logoC.svg'
+import logo from './logoo.svg'
+import add_icon from './add_icon.svg'
+import list_icon from './listIcon.svg'
+
+export const assets = {
+    logo,
+    add_icon,
+    list_icon,
+}
---
+++ admin/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { assets } from '../assets/assets'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+        <img  className='flex items-center w-[170px]' src={assets.logo} alt="Logo" />
+        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 rounded-full'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
