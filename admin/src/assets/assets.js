a8066861b298a74e7211687e4a391abfad00c9f1
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-02-15 21:25:43 +0000

    admin assets

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -2,9 +2,11 @@
 import logo from './logoo.svg'
 import add_icon from './add_icon.svg'
 import list_icon from './listIcon.svg'
+import upload_area from './upload_area.svg'
 
 export const assets = {
     logo,
     add_icon,
     list_icon,
+    upload_area,
 }
