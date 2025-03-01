84dbac161cc3342a9faa3d20e42340bf1742ea83
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-03-01 17:37:56 +0000

    logout implemented

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { assets } from '../assets/assets'
 
-const Navbar = () => {
+const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <img  className='flex items-center w-[170px]' src={assets.logo} alt="Logo" />
-        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 rounded-full'>Logout</button>
+        <button onClick={() => setToken('')} className='bg-gray-600 text-white px-5 py-2 sm:px-7 rounded-full'>Logout</button>
     </div>
   )
 }
