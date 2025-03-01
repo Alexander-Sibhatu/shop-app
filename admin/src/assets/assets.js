daeabbbae270330b1c3262596c5118464dcf5998
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-03-01 17:30:07 +0000

    parcel-icon

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -3,10 +3,12 @@ import logo from './logoo.svg'
 import add_icon from './add_icon.svg'
 import list_icon from './listIcon.svg'
 import upload_area from './upload_area.svg'
+import parcel_icon from './parcel_icon.svg'
 
 export const assets = {
     logo,
     add_icon,
     list_icon,
     upload_area,
+    parcel_icon,
 }
