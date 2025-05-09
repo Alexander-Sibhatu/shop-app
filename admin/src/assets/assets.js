538f90c74fbeecb30a7a6beab57d452a490e32c7
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-05-09 17:17:00 +0100

    Edit icon's name to match the original

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,7 +1,7 @@
 // import logo from './logoC.svg'
 import logo from './logoo.svg'
 import add_icon from './add_icon.svg'
-import list_icon from './listIcon.svg'
+import list_icon from './ListIcon.svg'
 import upload_area from './upload_area.svg'
 import parcel_icon from './parcel_icon.svg'
 
