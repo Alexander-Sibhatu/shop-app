c1b7810eaa1f7ec391644a0137468bae9ca82682
Alexander Sibhatu <sibhatualexander@gmail.com>
2025-05-10 18:11:15 +0100

    add link to to footer elements

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
+    
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        })
+    }
   return (
     <div>
         <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
             <div>
-                <img src={assets.logo} className='mb-5 w-32' alt="" />
+                <Link to='/' onClick={scrollToTop}><img src={assets.logo} className='mb-5 w-32' alt="" /></Link>
                 <p className='w-full md:w-2/3 text-gray-600'>
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero sed eius officia! Fuga corporis est blanditiis necessitatibus, reiciendis dolore ratione?
                 </p>
@@ -14,10 +22,10 @@ const Footer = () => {
             <div>
                 <p className='text-xl font-medium mb-5'>COMPANY</p>
                 <ul className='flex flex-col gap-1 text-gray-600'>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy policy</li>
+                    <li><Link to='/' onClick={scrollToTop}>Home</Link></li>
+                    <li><Link to='/about' onClick={scrollToTop}>About us</Link></li>
+                    <li><Link>Delivery</Link></li>
+                    <li><Link>Privacy policy</Link></li>
                 </ul>
             </div>
 
